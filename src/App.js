72102edf473bcd0577ab69f7f12b75fa5ecc7066
senju_hashiram <az9901204859@gmail.com>
2022-11-27 22:21:04 +0530

    done with the basic ui

--- src/App.js
+++ src/App.js
@@ -1,25 +1,13 @@
-import logo from './logo.svg';
-import './App.css';
-
-function App() {
+import { useState } from "react";
+import Dashboard from "./components/dashboard";
+import StockContext from "./components/stockContex";
+export default function App() {
+  const [stockSymbol,setStockSymbol]=useState("GOOG")
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    <StockContext.Provider value={{stockSymbol,setStockSymbol}}>
+<Dashboard></Dashboard>
+    </StockContext.Provider>
+    
+  )
 }
 
-export default App;
---
+++ tailwind.config.js
@@ -0,0 +1,21 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content:[
+    "./src/**/*.{js,jsx,ts,tsx}",
+  ],
+  theme: {
+    extend: {
+      colours:{
+        white:"white",
+        none:"none"
+      },
+      borderWidth:{1:"1px"},
+      gridTemplateRows:{
+        7:"repeat(7,minmax(0,1fr))",
+        8:"repeat(8,minmax(0,1fr))",
+      }
+
+    },
+  },
+  plugins: [],
+}
